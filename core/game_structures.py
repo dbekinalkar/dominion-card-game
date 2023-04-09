07a0f1ec2b496ffd85e70543eae224cb579b7ce1
Daniyal Bekinalkar <80331663+dbekinalkar@users.noreply.github.com>
2023-04-09 02:57:15 -0400

    More basic game structures

---
+++ core/game_structures.py
@@ -0,0 +1,30 @@
+class Player:
+    pass
+
+
+class Turn:
+    pass
+
+
+class Pile:
+    pass
+
+
+class Deck(Pile):
+    pass
+
+
+class Battlefield:
+    pass
+
+
+class Hand:
+    pass
+
+
+class Supply:
+    pass
+
+
+class Kingdom:
+    pass
